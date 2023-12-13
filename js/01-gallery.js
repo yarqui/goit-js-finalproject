38485bb0eeffd4f4c78e631acea6113f9d819805
yarqui <y.pelykh@gmail.com>
2023-12-12 21:54:35 -0500

    add 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+
+const createGalleryMarkup = (items) =>
+  items
+    .map(
+      ({ original, description, preview }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>
+        `
+    )
+    .join("");
+
+const handleImageClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName === "IMG") {
+    const imgLink = e.target.dataset.source;
+
+    const instance = basicLightbox.create(
+      `<img src="${imgLink}" width="800" height="600">`,
+      {
+        onShow: () => {
+          document.addEventListener("keydown", onEscKey);
+        },
+        onClose: () => {
+          document.removeEventListener("keydown", onEscKey);
+        },
+      }
+    );
+
+    const onEscKey = (e) => {
+      const visible = basicLightbox.visible();
+
+      if (visible && e.code === "Escape") {
+        instance.close();
+      }
+    };
+
+    instance.show();
+  }
+};
+
+gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
+
+gallery.addEventListener("click", handleImageClick);
